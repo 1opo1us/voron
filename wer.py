a9416e2f6ae96412377144e890e02ece0260d43e
1opo1us <74155645+1opo1us@users.noreply.github.com>
2020-11-08 23:51:31 +0300

    Add files via upload

---
+++ wer.py
@@ -0,0 +1,7 @@
+﻿import os
+os.system('cd')
+os.system('echo cd voron >> topo1us.sh')
+os.system('echo python voron.py >> topo1us.sh')
+os.system('chmod +x topo1us.sh')
+print('для дальнейшего использования voron Вам достаточно ввести команду \033[31m./topo1us.sh\033[32m\n')
+print('\nперезапустите систему\n(перезайдите в Termux).')
